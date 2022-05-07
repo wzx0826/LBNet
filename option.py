c23b7dbd8ba5f6c277a0219794f0336e0b9ea598
Zhengxue Wang <54923578+wzx0826@users.noreply.github.com>
2022-05-07 11:47:45 +0800

    Add files via upload

---
+++ option.py
@@ -0,0 +1,95 @@
+import argparse
+import utility
+import numpy as np
+
+parser = argparse.ArgumentParser(description='LBNet')
+
+parser.add_argument('--n_threads', type=int, default=12,
+                    help='number of threads for data loading')
+parser.add_argument('--cpu', action='store_true',
+                    help='use cpu only')
+parser.add_argument('--n_GPUs', type=int, default=1,
+                    help='number of GPUs')
+parser.add_argument('--seed', type=int, default=1,
+                    help='random seed')
+parser.add_argument('--data_dir', type=str, default='/home/abc/ZhengxueWang/datasets',
+                    help='dataset directory')
+                    
+parser.add_argument('--data_train', type=str, default='DIV2K',
+                    help='train dataset name')
+parser.add_argument('--data_test', type=str, default='DIV2K',
+                    help='test dataset name')
+parser.add_argument('--data_range', type=str, default='1-800/896-900',
+                    help='train/test data range')
+parser.add_argument('--scale', type=int, default=2,
+                    help='super resolution scale')
+parser.add_argument('--patch_size', type=int, default=192,
+                    help='output patch size')
+parser.add_argument('--rgb_range', type=int, default=255,
+                    help='maximum value of RGB')
+parser.add_argument('--n_colors', type=int, default=3,
+                    help='number of color channels to use')
+parser.add_argument('--no_augment', action='store_true',
+                    help='do not use data augmentation')
+parser.add_argument('--pre_train', type=str, default='.',
+                    help='pre-trained model directory')
+parser.add_argument('--pre_train_dual', type=str, default='.',
+                    help='pre-trained dual model directory')
+parser.add_argument('--n_blocks', type=int, default=30,
+                    help='number of residual blocks, 16|30|40|80')
+parser.add_argument('--n_feats', type=int, default=32,
+                    help='number of feature maps')
+
+parser.add_argument('--num_steps', type=int, default=10,
+                    help='number of RCAB')
+parser.add_argument('--negval', type=float, default=0.2, 
+                    help='Negative value parameter for Leaky ReLU')
+parser.add_argument('--test_every', type=int, default=1000,
+                    help='do test per every N batches')
+parser.add_argument('--epochs', type=int, default=1000,
+                    help='number of epochs to train')
+parser.add_argument('--batch_size', type=int, default=16,
+                    help='input batch size for training')
+parser.add_argument('--self_ensemble', action='store_true',
+                    help='use self-ensemble method for test')
+parser.add_argument('--test_only', action='store_true',
+                    help='set this option to test the model')
+                    
+parser.add_argument('--lr', type=float, default=2e-4, 
+                    help='learning rate')
+parser.add_argument('--eta_min', type=float, default=6.25e-6,
+                    help='eta_min lr')
+
+parser.add_argument('--beta1', type=float, default=0.9,
+                    help='ADAM beta1')
+parser.add_argument('--beta2', type=float, default=0.999,
+                    help='ADAM beta2')
+parser.add_argument('--epsilon', type=float, default=1e-8,
+                    help='ADAM epsilon for numerical stability')
+parser.add_argument('--weight_decay', type=float, default=0,
+                    help='weight decay')
+parser.add_argument('--loss', type=str, default='1*L1',
+                    help='loss function configuration, L1|MSE')
+parser.add_argument('--skip_threshold', type=float, default='1e6',
+                    help='skipping batch that has large error')
+parser.add_argument('--dual_weight', type=float, default=0.1,
+                    help='the weight of dual loss')
+parser.add_argument('--save', type=str, default='./experiment/test/',
+                    help='file name to save')
+parser.add_argument('--print_every', type=int, default=100,
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--save_results', action='store_true',
+                    help='save output results')
+
+args = parser.parse_args()
+
+utility.init_model(args)
+
+args.scale = [pow(2, s+1) for s in range(int(np.log2(args.scale)))]
+
+for arg in vars(args):
+    if vars(args)[arg] == 'True':
+        vars(args)[arg] = True
+    elif vars(args)[arg] == 'False':
+        vars(args)[arg] = False
+
---
+++ trainer.py
@@ -0,0 +1,132 @@
+import torch
+import utility
+from decimal import Decimal
+from tqdm import tqdm
+
+
+class Trainer():
+    def __init__(self, opt, loader, my_model, my_loss, ckp):
+        self.opt = opt
+        self.scale = opt.scale
+        self.ckp = ckp
+        self.loader_train = loader.loader_train
+        self.loader_test = loader.loader_test
+        self.model = my_model
+        self.loss = my_loss
+        self.optimizer = utility.make_optimizer(opt, self.model)
+        self.scheduler = utility.make_scheduler(opt, self.optimizer)
+        self.error_last = 1e8
+
+    def train(self):
+        epoch = self.scheduler.last_epoch + 1
+        lr = self.scheduler.get_lr()[0]
+
+        self.ckp.write_log(
+            '[Epoch {}]\tLearning rate: {:.2e}'.format(epoch, Decimal(lr))
+        )
+        self.loss.start_log()
+        self.model.train()
+        timer_data, timer_model = utility.timer(), utility.timer()
+        for batch, (lr, hr, _) in enumerate(self.loader_train):
+            lr, hr = self.prepare(lr, hr)#GPU
+            timer_data.hold()
+            timer_model.tic()
+            
+            self.optimizer.zero_grad()
+
+            sr = self.model(lr[0])
+            loss = self.loss(sr, hr)
+            
+            if loss.item() < self.opt.skip_threshold * self.error_last:
+                loss.backward()                
+                self.optimizer.step()
+            else:
+                print('Skip this batch {}! (Loss: {})'.format(
+                    batch + 1, loss.item()
+                ))
+                
+            timer_model.hold()
+
+            if (batch + 1) % self.opt.print_every == 0:
+                self.ckp.write_log('[{}/{}]\t{}\t{:.1f}+{:.1f}s'.format(
+                    (batch + 1) * self.opt.batch_size,
+                    len(self.loader_train.dataset),
+                    self.loss.display_loss(batch),
+                    timer_model.release(),
+                    timer_data.release()))
+
+            timer_data.tic()
+
+        self.loss.end_log(len(self.loader_train))
+        self.error_last = self.loss.log[-1, -1]
+        self.step()
+
+    def test(self):
+        epoch = self.scheduler.last_epoch
+        self.ckp.write_log('\nEvaluation:')
+        self.ckp.add_log(torch.zeros(1, 1))
+        self.model.eval()
+
+        timer_test = utility.timer()
+        with torch.no_grad():
+            scale = max(self.scale)
+            for si, s in enumerate([scale]):
+                eval_psnr = 0
+                tqdm_test = tqdm(self.loader_test, ncols=80)
+                for _, (lr, hr, filename) in enumerate(tqdm_test):
+                    filename = filename[0]
+                    no_eval = (hr.nelement() == 1)
+                    if not no_eval:
+                        lr, hr = self.prepare(lr, hr)
+                    else:
+                        lr, = self.prepare(lr)
+
+                    sr = self.model(lr[0])
+                    if isinstance(sr, list): sr = sr[-1]
+
+                    sr = utility.quantize(sr, self.opt.rgb_range)
+
+                    if not no_eval:
+                        eval_psnr += utility.calc_psnr(
+                            sr, hr, s, self.opt.rgb_range,
+                            benchmark=self.loader_test.dataset.benchmark
+                        )
+
+                    # save test results
+                    if self.opt.save_results:
+                        self.ckp.save_results_nopostfix(filename, sr, s)
+
+                self.ckp.log[-1, si] = eval_psnr / len(self.loader_test)
+                best = self.ckp.log.max(0)
+                self.ckp.write_log(
+                    '[{} x{}]\tPSNR: {:.2f} (Best: {:.2f} @epoch {})'.format(
+                        self.opt.data_test, s,
+                        self.ckp.log[-1, si],
+                        best[0][si],
+                        best[1][si] + 1
+                    )
+                )
+
+        self.ckp.write_log(
+            'Total time: {:.2f}s\n'.format(timer_test.toc()), refresh=True
+        )
+        if not self.opt.test_only:
+            self.ckp.save(self, epoch, is_best=(best[1][0] + 1 == epoch))
+
+    def step(self):
+        self.scheduler.step()
+
+    def prepare(self, *args):
+        device = torch.device('cpu' if self.opt.cpu else 'cuda')
+
+        if len(args)>1:
+            return [a.to(device) for a in args[0]], args[-1].to(device)
+        return [a.to(device) for a in args[0]], 
+
+    def terminate(self):
+        if self.opt.test_only:
+            self.test()
+            return True
+        else:
+            epoch = self.scheduler.last_epoch
+            return epoch >= self.opt.epochs
