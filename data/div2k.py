aed1902993e2e338f6ff6db74337ea72543563cf
Zhengxue Wang <54923578+wzx0826@users.noreply.github.com>
2022-05-07 11:52:21 +0800

    Add files via upload

---
+++ data/div2k.py
@@ -0,0 +1,16 @@
+import os
+from data import srdata
+
+
+class DIV2K(srdata.SRData):
+    def __init__(self, args, name='DIV2K', train=True, benchmark=False):
+        super(DIV2K, self).__init__(
+            args, name=name, train=train, benchmark=benchmark
+        )
+
+    def _set_filesystem(self, data_dir):
+        super(DIV2K, self)._set_filesystem(data_dir)
+        self.dir_hr = os.path.join(self.apath, 'DIV2K_train_HR')
+        self.dir_lr = os.path.join(self.apath, 'DIV2K_train_LR_bicubic')
+
+
